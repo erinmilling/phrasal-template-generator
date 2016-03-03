a3ff3fcc48b503675e9d486b929591307c925039
erinmilling <mill0814@algonquinlive.com>
2016-03-02 21:52:58 -0500

    added separate vars

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
-  var newStory = alert('Lets make a story together!');
+var newStory = alert('Lets make a story together!');
+
+var colour = prompt('A colour (orange, indigo,etc.)');
+
+var creature = prompt('A creature (dragon, moth, etc.)');
+
+var adjective = prompt('An adjective (beautiful, super, etc.)');
+
+var verb = prompt('A past tense verb (ran, burped, etc.)');
+
+var story = prompt('Which story would you like? \n a) Snacking \n b) Escaping \n c) Winning \n Enter the letter below:');
