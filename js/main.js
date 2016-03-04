b5b58b402a4d93dc5ac012b52c0fce6bd7076c92
erinmilling <mill0814@algonquinlive.com>
2016-03-03 19:21:56 -0500

    added story b

--- js/main.js
+++ js/main.js
@@ -11,8 +11,11 @@ var verb = prompt('A past tense verb (ran, burped, etc.)');
 var story = prompt('Which story would you like? \n a) Snacking \n b) Escaping \n c) Winning \n Enter the letter below:');
 
 if (story == 'a'){
-  document.write('After snacking on' + adjective + 'treats, the' + colour + 'bellied' + creature + '' + verbed 'for hours.' }
+  document.write('After snacking on' + adjective + 'treats, the' + colour + 'bellied' + creature + '' + verbed 'for hours.'
+}
 
-Ameilia {verbed} through the {adjective} {colour} nebula escaping the space {creature}.
+if (story == 'b') {
+  document.write('Ameilia ' + verb + ' through the ' + adjective + ' ' + colour + ' nebula escaping the space ' + creature + '.');
+}
 
 Jackson chose his {adjective}, {colour} {creature} card and {verbed} it to the table knowing he won.
