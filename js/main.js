242a60194b3d06462a73f039bebf146dfe7e6436
erinmilling <mill0814@algonquinlive.com>
2016-03-03 19:19:52 -0500

    if story a added

--- js/main.js
+++ js/main.js
@@ -10,7 +10,8 @@ var verb = prompt('A past tense verb (ran, burped, etc.)');
 
 var story = prompt('Which story would you like? \n a) Snacking \n b) Escaping \n c) Winning \n Enter the letter below:');
 
-After snacking on {adjective} treats, the {colour} bellied {creature} {verbed} for hours.
+if (story == 'a'){
+  document.write('After snacking on' + adjective + 'treats, the' + colour + 'bellied' + creature + '' + verbed 'for hours.' }
 
 Ameilia {verbed} through the {adjective} {colour} nebula escaping the space {creature}.
 
