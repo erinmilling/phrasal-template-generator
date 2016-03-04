e758822823a5094be1c209fa1515529fa0e3d2bb
erinmilling <mill0814@algonquinlive.com>
2016-03-03 19:06:23 -0500

    added context to doc

--- js/main.js
+++ js/main.js
@@ -9,3 +9,9 @@ var adjective = prompt('An adjective (beautiful, super, etc.)');
 var verb = prompt('A past tense verb (ran, burped, etc.)');
 
 var story = prompt('Which story would you like? \n a) Snacking \n b) Escaping \n c) Winning \n Enter the letter below:');
+
+After snacking on {adjective} treats, the {colour} bellied {creature} {verbed} for hours.
+
+Ameilia {verbed} through the {adjective} {colour} nebula escaping the space {creature}.
+
+Jackson chose his {adjective}, {colour} {creature} card and {verbed} it to the table knowing he won.
